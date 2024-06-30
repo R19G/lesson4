09806158fb35c9624897efaf7f15ed008e933b95
R19G <guseynov_rustam2017@mail.ru>
2024-06-30 18:17:45 +0300

    Update homework4.py

--- homework4.py
+++ homework4.py
@@ -1,6 +1,7 @@
-my_string = "Организация программ и методы строк."
-print(my_string .upper())
-print(my_string .lower())
+my_string = input()
+print(my_string.upper())
+print(my_string.lower())
 print(my_string .replace(" ", ""))
 print(my_string[0])
 print(my_string[-1])
+print(len(my_string))
